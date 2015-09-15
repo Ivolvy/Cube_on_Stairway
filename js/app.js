a536279d0a207d528100bddb94a1d3cb5337956d
MichaelGenty <michael.genty@free.fr>
2015-09-15 16:46:01 +0200

    Add the stairways animations

--- js/app.js
+++ js/app.js
@@ -83,28 +83,63 @@ StairwayCubes.prototype.createBoxes = function(){
     var material = new THREE.MeshLambertMaterial( { color: 0xf2f2f2, shading: THREE.FlatShading});
 
 
+    this.boxeContainer = new THREE.Object3D();
+    this.boxeContainer.rotation.x = -2*Math.PI;
+    this.boxeContainer.position.y = 50;
+    this.boxeContainer.position.z = -50;
 
-    this.temp = new THREE.Object3D();
-    this.temp.position.x = 0;
-    this.temp.position.y = 50;
-    this.temp.position.z = -50;
-    that.temp.rotation.x = -2*Math.PI;
 
     this.boxe = new THREE.Mesh( geometry, material );
     this.boxe.position.x = 0;
     this.boxe.position.y = -25;
     this.boxe.position.z = 25;
 
-    this.temp.add(this.boxe);
-    this.scene.add(this.temp);
+    this.boxeContainer.add(this.boxe);
+    this.scene.add(this.boxeContainer);
+
+
+    var geometryStair = new THREE.BoxGeometry( 50, 51, 50 );
+    geometryStair.applyMatrix(new THREE.Matrix4().makeTranslation(0, 25, 0)); //permit to change the origin point to the box floor
+    this.stair1 = new THREE.Mesh( geometryStair, material );
+    this.stair1.position.y = -1;
+    this.stair1.position.z = -75;
+    this.stair1.scale.y = 0;
+
+    this.stair2 = new THREE.Mesh( geometryStair, material );
+    this.stair2.position.y = -1;
+    this.stair2.position.z = -125;
+    this.stair2.scale.y = 0;
+
+    this.stair3 = new THREE.Mesh( geometryStair, material );
+    this.stair3.position.y = -1;
+    this.stair3.position.z = -175;
+    this.stair3.scale.y = 0;
+
+    this.stair4 = new THREE.Mesh( geometryStair, material );
+    this.stair4.position.y = -1;
+    this.stair4.position.z = -225;
+    this.stair4.scale.y = 0;
+
+    this.stair5 = new THREE.Mesh( geometryStair, material );
+    this.stair5.position.y = -1;
+    this.stair5.position.z = -275;
+    this.stair5.scale.y = 0;
+
+
+
+    this.scene.add(this.stair1);
+    this.scene.add(this.stair2);
+    this.scene.add(this.stair3);
+    this.scene.add(this.stair4);
+    this.scene.add(this.stair5);
 };
 
 StairwayCubes.prototype.createFloor = function(){
-    var geometry2 = new THREE.PlaneGeometry( 500, 500); //use PlaneBufferGeometry  ? todo
+    var geometry2 = new THREE.PlaneBufferGeometry( 1000, 1000);
     var material2 = new THREE.MeshBasicMaterial( { color: 0xf2f2f2 } );
     var floor = new THREE.Mesh( geometry2, material2 );
     floor.material.side = THREE.DoubleSide;
-    floor.position.y =-180;
+    floor.position.y =0;
     floor.rotation.x = 90*Math.PI/180;
     floor.rotation.y = 0;
     floor.rotation.z = 0;
@@ -131,24 +166,82 @@ StairwayCubes.prototype.createLights = function(){
 };
 
 StairwayCubes.prototype.animateBoxes = function(){
+    this.tl = new TimelineMax({repeat: 4 , delay: 1.5, repeatDelay:0, onComplete:this.reverseAnimation});
+    this.tl.to(this.boxeContainer.rotation, 0.8, {x: -3*Math.PI, ease: Power1.easeInOut, onComplete:this.increaseValue});
 
-    var tl = new TimelineMax({repeat: 4 ,repeatDelay:0});
-    tl.to(this.temp.rotation, 1, {x: -3*Math.PI, ease: Power1.easeInOut, onComplete:this.increaseValue});
+
+    this.t2 = new TimelineMax({repeat: 0 , delay: 1, repeatDelay:0});
+    this.t2.to(this.stair1.scale, 0.8, {y: 1, ease: Expo.easeInOut});
+    this.t2.to(this.stair2.scale, 0.8, {y: 2, ease: Expo.easeInOut});
+
+    this.t2.to(this.stair3.scale, 0.8, {y: 3, ease: Expo.easeInOut});
+    //this.t2.to(this.stair1.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-1");
+
+    this.t2.to(this.stair4.scale, 0.8, {y: 4, ease: Expo.easeInOut});
+    //this.t2.to(this.stair2.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-1");
+
+    this.t2.to(this.stair5.scale, 0.8, {y: 5, ease: Expo.easeInOut});
+    //this.t2.to(this.stair3.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-1");
+    //this.t2.to(this.stair4.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-0.4");
+
 
 };
 
 StairwayCubes.prototype.increaseValue = function(){
-    that.temp.position.y += 50;
-    that.temp.position.z -= 50;
+    that.boxeContainer.position.y += 50;
+    that.boxeContainer.position.z -= 50;
 
     that.boxe.position.y = -25;
     that.boxe.position.z = 25;
 
-    that.temp.rotation.x = -2*Math.PI;
+    that.boxeContainer.rotation.x = -2*Math.PI;
+};
+StairwayCubes.prototype.decreaseValue = function(){
+    that.boxeContainer.position.y -= 50;
+    that.boxeContainer.position.z += 50;
+
+    that.boxe.position.y = +25;
+    that.boxe.position.z = -25;
+
+    that.boxeContainer.rotation.x = 2*Math.PI;
+};
+
+StairwayCubes.prototype.resetAnimations = function(){
+    that.increaseValue();
+
+    setTimeout(function(){
+        that.tl.restart();
+        that.t2.restart();
+    }, 2000);
+
+};
+
+StairwayCubes.prototype.reverseAnimation = function(){
+    that.decreaseValue();
+    that.t3 = new TimelineMax({repeat: 4 , delay: 1.5, repeatDelay:0, onComplete:that.resetAnimations});
+    that.t3.to(that.boxeContainer.rotation, 0.8, {x: 3*Math.PI, ease: Power1.easeInOut, onComplete:that.decreaseValue});
+
+    this.t4 = new TimelineMax({repeat: 0 , delay: 0.5, repeatDelay:0});
+    this.t4.to(that.stair5.scale, 0.8, {y: 5, ease: Expo.easeInOut});
+    this.t4.to(that.stair4.scale, 0.8, {y: 4, ease: Expo.easeInOut});
+
+    this.t4.to(that.stair3.scale, 0.8, {y: 3, ease: Expo.easeInOut});
+    this.t4.to(that.stair5.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-1");
+
+    this.t4.to(that.stair2.scale, 0.8, {y: 2, ease: Expo.easeInOut});
+    this.t4.to(that.stair4.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-1");
+
+    this.t4.to(that.stair1.scale, 0.8, {y: 1, ease: Expo.easeInOut});
+    this.t4.to(that.stair3.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-1");
+    this.t4.to(that.stair2.scale, 0.8, {y: 0, ease: Back.easeInOut},"=-0.4");
+
+    this.t4.to(that.stair1.scale, 0.8, {y: 0, ease: Back.easeInOut});
+
+
 };
 
 StairwayCubes.prototype.render = function(){
-  //  this.temp.rotation.x -= 2*Math.PI/128;
+  //  this.boxeContainer.rotation.x -= 2*Math.PI/128;
 
     requestAnimationFrame(this.render.bind(this));
     this.renderer.render(this.scene, this.camera);
